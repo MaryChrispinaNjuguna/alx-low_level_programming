a2b640405f1944641a4a6ee214f28f7bf3b20ce0
MaryChrispinaNjuguna <chrispinamary@gmail.com>
2023-04-19 13:44:05 -0700

    Pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,26 +1,61 @@
-#ifndef CALC_H
-#define CALC_H
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "3-calc.h"
 
 /**
- * struct op - Struct op
- * @op: The operator
- * @f: The associated function
+ * op_add - Returns the sum of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: The sum of a and b
  */
-typedef struct op
+int op_add(int a, int b)
 {
-	char *op;
-	int (*f)(int a, int b);
-} op_t;
+	return (a + b);
+}
 
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
+/**
+ * op_sub - Returns the difference of two numbers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
 
-#endif
+/**
+ * op_mul - Returns the product of two numbers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: The product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Returns the division of two numbers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: The quotient of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Returns the remainder of the division of two numbers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: The remainder of the division of a by b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
